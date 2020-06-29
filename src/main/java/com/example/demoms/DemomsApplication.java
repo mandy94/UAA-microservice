886a4aa2f17fbea6b3276458c568141e06674d27
mandy94 <majasavic199@gmail.com>
2020-06-29 22:57:56 +0200

    Init commit

---
+++ src/main/java/com/example/demoms/DemomsApplication.java
@@ -0,0 +1,13 @@
+package com.example.demoms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemomsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemomsApplication.class, args);
+	}
+
+}
